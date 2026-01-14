c0efb41edb075c4082a69fd66ab495bfdf7ab3f8
Wandeson Ricardo <wsricardo@users.noreply.github.com>
2026-01-14 10:15:34 -0300

    Dynamically load blog posts from Atom feed
    Refactored blog.html to fetch and display blog posts using JavaScript from an Atom feed, replacing static post markup. Added the Atom feed at blog/feeds/all.atom.xml and updated pelicanconf.py to generate the feed. Also removed 'Gerado com Pelican' from footers in cartum.html, galeria.html, index.html, and projetos.html.

--- pelicanconf.py
+++ pelicanconf.py
@@ -14,7 +14,8 @@ TIMEZONE = 'America/Sao_Paulo'
 DEFAULT_LANG = 'pt-br'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
